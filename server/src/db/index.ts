f1b288c59561813a54eb2147f2d0d728a7499d04
Arghya <arghyadas242004@gmail.com>
2023-12-29 19:04:20 +0530

    Prod upload

--- server/src/db/index.ts
+++ server/src/db/index.ts
@@ -1,10 +1,12 @@
+import 'dotenv/config'
 import mongoose , {ConnectOptions} from "mongoose";
 
+const DATABASE = process.env.DATABASE ;
 import UserModel from "./user";
 
 const connectDB = async () => {
     try{
-        await mongoose.connect('mongodb://127.0.0.1:27017/testDB',{
+        await mongoose.connect(DATABASE,{
             useNewUrlParser : true,
             useUnifiedTopology :true
         } as ConnectOptions)
